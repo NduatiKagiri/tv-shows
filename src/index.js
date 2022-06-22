27e37cf6c5c88186e51f92966198725826136cb6
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-06-23 00:36:51 +0300

    Add Likes functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './style.css';
 import Shows from './modules/getShows.js';
+import Likes from './modules/likes.js';
 
 Shows.getShows();
 
 Shows.displayShows();
+
+Likes.getLikes();
+
+const likeBtns = document.querySelectorAll('.likeButton');
+likeBtns.forEach((likeBtn) => {
+  likeBtn.addEventListener('click', () => {
+    const id = likeBtn.getAttribute('id').slice(4);
+    Likes.setLikes(id.toString());
+    Likes.getLikes();
+  });
+});
