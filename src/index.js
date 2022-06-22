639f3a420a10950ecac822ceb5cec3b47abf442a
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-06-23 01:57:35 +0300

    Add homepage items counter

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import Shows from './modules/getShows.js';
 Shows.getShows();
 
 Shows.displayShows();
+
+const itemsCounter = () => {
+  const shows = document.querySelectorAll('.show');
+  const items = shows.length;
+  document.getElementById('count').innerHTML = `<h4>Total number of shows is ( ${items} )</h4>`;
+};
+itemsCounter();
