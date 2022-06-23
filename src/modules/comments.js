415ffba564c60740b56e2e6711c7ba72b0e3f3bc
dqarias <dino.quispea@gmail.com>
2022-06-23 10:35:56 -0500

    Fix linter errors

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,36 +1,26 @@
-const url_involvement = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lt6GBu2WzXrJuUpzFb4d'
+const urlInvolvement = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lt6GBu2WzXrJuUpzFb4d';
 
 const getCommentsByItem = async (id) => {
-        const response = await fetch(`${url_involvement}/comments?item_id=item${id}`)
-        if(response.status !== 400){
-            const dataComment = await response.json();
-            console.log("datacomment",dataComment);
-            return dataComment;
-        }else{
-            return 0;
-        }      
-}
-
-
-/* if (response.status !== 400) {    
-    const results = await response.json();   
-     results.map((item) => {      
-        if (item.item_id != null) {        
-            document.getElementById(`likes${item.item_id}`).innerHTML = `${item.likes} Likes`;  
-}      return true;    });  } */
+  const response = await fetch(`${urlInvolvement}/comments?item_id=item${id}`);
+  if (response.status !== 400) {
+    const dataComment = await response.json();
+    return dataComment;
+  }
+  return 0;
+};
 
 const addComment = async (id, username, comment) => {
-    const response = await fetch(`${url_involvement}/comments`, {
-        method: 'POST',
-        body: JSON.stringify({
-          item_id: id, 
-          username: username, 
-          comment: comment
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-}
+  await fetch(`${urlInvolvement}/comments`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      username,
+      comment,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
 
-export {getCommentsByItem, addComment};
+export { getCommentsByItem, addComment };
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,6 +1,6 @@
 import myURL from './baseurl.js';
-import {addComment, getCommentsByItem} from './comments.js';
-import {htmlModalTvShow } from './htmlPopup.js'
+import { addComment, getCommentsByItem } from './comments.js';
+import htmlModalTvShow from './htmlPopup.js';
 
 const getTvShowById = async (id) => {
   const response = await fetch(`${myURL}/${id}`);
@@ -9,14 +9,13 @@ const getTvShowById = async (id) => {
 };
 
 const getAllComments = async (id) => {
-  const comments =document.querySelector('.modal__comments')
-    comments.innerHTML='';
-    const getComments = await getCommentsByItem(id);
-    if(getComments !== 0){
-      getComments.forEach((comment)=>{
-        console.log(comment)
-        comments.innerHTML += 
-        ` <div class="comments">
+  const comments = document.querySelector('.modal__comments');
+  comments.innerHTML = '';
+  const getComments = await getCommentsByItem(id);
+  if (getComments !== 0) {
+    getComments.forEach((comment) => {
+      comments.innerHTML
+        += ` <div class="comments">
         <div class="comments__header">
           <p class="comments__username">${comment.username}</p>
           <p class="comments__date">${comment.creation_date}</p>
@@ -25,14 +24,13 @@ const getAllComments = async (id) => {
           <p class="comments__detail">${comment.comment}</p>
         </div>
       </div>`;
-       })  
-    }
-}
+    });
+  }
+};
 
 const addNewComment = (id) => {
-  const addComments = document.querySelector('.modal__add-comment')
-  addComments.innerHTML = 
-  `<div class="add-comment__container">
+  const addComments = document.querySelector('.modal__add-comment');
+  addComments.innerHTML = `<div class="add-comment__container">
   <input class="add-comment__user" type="text" id="username" name="username" placeholder="Write your Name" required/>
   <textarea
       class="add-comment__review"
@@ -40,21 +38,21 @@ const addNewComment = (id) => {
       rows="6"
       required></textarea>
   <button id="add_comment" class="btn btn-add" type="button">Submit</button>
- </div>`
+ </div>`;
 
- const btnAddComment = document.querySelector('#add_comment')
- const inputUserName = document.querySelector('#username')
- const inputReview = document.querySelector('.add-comment__review')
+  const btnAddComment = document.querySelector('#add_comment');
+  const inputUserName = document.querySelector('#username');
+  const inputReview = document.querySelector('.add-comment__review');
 
- btnAddComment.addEventListener('click', ()=>{
-  if (inputUserName.value && inputReview.value) {
-    addComment(`item${id}`,inputUserName.value, inputReview.value);
-  }
-  inputUserName.value = '';
-  inputReview.value = '';
-  setTimeout(() => getAllComments(id), 1000);
- })
-}
+  btnAddComment.addEventListener('click', () => {
+    if (inputUserName.value && inputReview.value) {
+      addComment(`item${id}`, inputUserName.value, inputReview.value);
+    }
+    inputUserName.value = '';
+    inputReview.value = '';
+    setTimeout(() => getAllComments(id), 1000);
+  });
+};
 
 const openModal = async (id) => {
   const tvShowId = await getTvShowById(id);
