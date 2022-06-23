44ada40bad7a9fab94ed8c0b1edf5e9a1ce8a8fb
dqarias <dino.quispea@gmail.com>
2022-06-23 12:47:52 -0500

    Add functionm to test comments counter

---
+++ src/tests/commentsCounter.test.js
@@ -0,0 +1,123 @@
+import commentsCounter from "../modules/commentsCounter";
+
+describe('Test for calculate the number of comments', ()=>{
+    test('Count zero elements', ()=>{
+        const comments = [
+            
+         ];
+      
+         expect(commentsCounter(comments)).toBe(0)
+    })
+    test('Count five elements', ()=>{
+        const comments = [
+            {
+              "username": "Ronald",
+              "creation_date": "2022-06-23",
+              "comment": "I like this!"
+            },
+            {
+              "username": "Dino",
+              "creation_date": "2022-06-23",
+              "comment": "I love this"
+            },
+            {
+              "username": "John",
+              "creation_date": "2022-06-23",
+              "comment": "The best!"
+            },
+            {
+              "username": "Nduati",
+              "creation_date": "2022-06-23",
+              "comment": "Amazing!"
+            },
+            {
+              "username": "Ronald",
+              "creation_date": "2022-06-23",
+              "comment": "I likes this!"
+            }
+         ];
+      
+         expect(commentsCounter(comments)).toBe(5)
+    })
+    test('Count fifteen elements', ()=>{
+        const comments = [
+            {
+              "username": "Ronald",
+              "creation_date": "2022-06-23",
+              "comment": "I like this!"
+            },
+            {
+              "username": "Dino",
+              "creation_date": "2022-06-23",
+              "comment": "I love this"
+            },
+            {
+              "username": "John",
+              "creation_date": "2022-06-23",
+              "comment": "The best!"
+            },
+            {
+              "username": "Nduati",
+              "creation_date": "2022-06-23",
+              "comment": "Amazing!"
+            },
+            {
+                "username": "Nduati",
+                "creation_date": "2022-06-23",
+                "comment": "Amazing!"
+            },
+            {
+                "username": "Nduati",
+                "creation_date": "2022-06-23",
+                "comment": "Amazing!"
+            },
+            {
+              "username": "Ronald",
+              "creation_date": "2022-06-23",
+              "comment": "I likes this!"
+            },
+            {
+                "username": "Ronald",
+                "creation_date": "2022-06-23",
+                "comment": "I like this!"
+            },
+            {
+                "username": "Dino",
+                "creation_date": "2022-06-23",
+                "comment": "I love this"
+            },
+            {
+                "username": "John",
+                "creation_date": "2022-06-23",
+                "comment": "The best!"
+            },
+            {
+                "username": "Nduati",
+                "creation_date": "2022-06-23",
+                "comment": "Amazing!"
+            },
+            {
+                  "username": "Nduati",
+                  "creation_date": "2022-06-23",
+                  "comment": "Amazing!"
+            },
+            {
+                  "username": "Nduati",
+                  "creation_date": "2022-06-23",
+                  "comment": "Amazing!"
+            },
+            {
+                "username": "Ronald",
+                "creation_date": "2022-06-23",
+                "comment": "I likes this!"
+            },
+            {
+                "username": "Ronald",
+                "creation_date": "2022-06-23",
+                "comment": "I likes this!"
+            }
+         ];
+      
+         expect(commentsCounter(comments)).toBe(15)
+    })
+})
