36e25ce020991df6fc00ca1a8d51b1ecd8c45dac
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-06-24 20:24:51 +0300

    Solve Likes bug

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 > TV Shows project: Fetch tv shows from an API and display them on a webpage. Store user's likes and comments in an API
 
 ## Built With
-
 - HTML, CSS, JS
 - ESLint, Stylelint, Web Hint, Lighthouse, Webpack
+
+## Online Version Link
+This project is available online through the following link.
+https://tv-shows.nduatikagiri.co.ke
 
 ## Getting Started
 To get a local copy up and running follow these simple example steps.
@@ -15,18 +18,16 @@ To get a local copy up and running follow these simple example steps.
 A text editor eg Atom
 
 ### Setup
-Download the code in ZIP file
-Install
-Unzip the code to a folder
-Usage
-Open the folder in a text editor
-Deployment
-Deploy on Github or Cloud
 
-or clone directly from Github using the following git command:
-```
-git clone https://github.com/NduatiKagiri/tv-shows.git
-```
+ **Step 1: Clone this repo locally using git**
+- Create a folder in your local machine
+- Open your console in your folder and run this command: `git clone https://github.com/NduatiKagiri/tv-shows.git`
+- Alternatively, you can just download the complete zip file and extract the folder in your directory
+
+ **Step 2: Run in your project directory**
+- Run `npm install` to install all the dependencies from the package.json
+- Run `npm run start` to open a page live server using webpack devServer
+- Run `npm run build` to build the project
 
 ## Authors
 :bust_in_silhouette: **NduatiKagiri**
--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ import ShowsCounter from './modules/itemsCounter.js';
 
 Shows.getShows();
 
+setInterval(() => { Likes.getLikes(); }, 500);
+
 setTimeout(() => {
   ShowsCounter.itemsCounter();
   Shows.displayShows();
-  Likes.getLikes();
-}, 3000);
 
-const likeBtns = document.querySelectorAll('.likeButton');
-likeBtns.forEach((likeBtn) => {
-  likeBtn.addEventListener('click', () => {
-    const id = likeBtn.getAttribute('id');
-    const showID = id.slice(4);
-    Likes.setLikes(showID);
-    likeBtn.style.display = 'none';
-    const likedID = `liked${showID.toString()}`;
-    const likedBtn = document.getElementById(likedID);
-    likedBtn.style.animation = 'displayliked 1.5s ease-out forwards 0.1s';
-    likedBtn.style.display = 'block';
-    likedBtn.style.animation = 'removeliked 0.7s ease-in forwards 1.1s';
-    setTimeout(() => {
-      likedBtn.style.display = 'none';
-      likeBtn.style.display = 'block';
-    }, 1000);
-    setTimeout(Likes.getLikes(), 1000);
+  const likeBtns = document.querySelectorAll('.likeButton');
+  likeBtns.forEach((likeBtn) => {
+    likeBtn.addEventListener('click', () => {
+      const id = likeBtn.getAttribute('id');
+      const showID = id.slice(4);
+      Likes.setLikes(showID);
+      likeBtn.style.display = 'none';
+      const likedID = `liked${showID.toString()}`;
+      const likedBtn = document.getElementById(likedID);
+      likedBtn.style.animation = 'displayliked 1.5s ease-out forwards 0.1s';
+      likedBtn.style.display = 'block';
+      likedBtn.style.animation = 'removeliked 0.7s ease-in forwards 1.1s';
+      setTimeout(() => {
+        likedBtn.style.display = 'none';
+        likeBtn.style.display = 'block';
+      }, 1000);
+      setTimeout(Likes.getLikes(), 3000);
+    });
   });
-});
+}, 2000);
